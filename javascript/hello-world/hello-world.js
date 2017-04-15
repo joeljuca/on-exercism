b230e71bb20f51cbfe9e918d0599292730062767
Joel Jucá <joeljuca@proton.me>
2017-04-15 19:51:40 -0300

    javascript/hello-world

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,13 +1,11 @@
-//
-// This is a stub file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at hello-world.spec.js--that should give you some hints about what is
-// expected here.
+function HelloWorld () {}
 
-var HelloWorld = function () {};
+HelloWorld.prototype.hello = function (input) {
+  input = input
+    ? input
+    : 'World'
+  
+  return 'Hello, ' + input + '!'
+}
 
-HelloWorld.prototype.hello = function () {
-  return 'Hello, World!';
-};
-
-module.exports = HelloWorld;
+module.exports = HelloWorld
